bfe96e975ba04c04ec6b841ff9b6f1e51c58b610
Lacey Weaver <jlweave89@gmail.com>
2022-11-14 11:59:54 -0700

    Add final commit for IC ran out of time

--- lib/ride.rb
+++ lib/ride.rb
@@ -13,15 +13,33 @@ class Ride
     @admission_fee = info[:admission_fee]
     @excitement = info[:excitement]
     @total_revenue = 0
-    @rider_log = Hash.new(0)
+    @rider_log = Hash.new
   end
 
   def board_rider(visitor)
-    if visitor.name == visitor.name
-      @rider_log[visitor] += 1
-    else
+    if visitor.tall_enough?(min_height) && visitor.preference_check(@excitement)
+      if @rider_log[visitor] == nil
       @rider_log[visitor] = 1
+      else visitor.name == visitor.name
+      @rider_log[visitor] += 1
+      end
+      @total_revenue += @admission_fee
+      visitor.spend(@admission_fee)
+    else
+      false
     end
-    @rider_log
   end
+
+  # def board_rider(visitor)
+  #   if visitor.name == visitor.name
+  #     @rider_log[visitor] += 1
+  #   else
+  #     @rider_log[visitor] = 1
+  #   end
+  #     @rider_log
+  # end
+ #says that everything is passing even the unfinsihed method... sus
+
+
+ 
 end
--- lib/visitor.rb
+++ lib/visitor.rb
@@ -22,4 +22,20 @@ class Visitor
       false
     end
   end
+
+    def preference_check(rides_pref)
+      count = 0
+      @preferences.each do |pref|
+        if rides_pref == pref
+          count += 1
+        end
+      if count > 0
+        true
+      end
+    end
+  end
+
+  def spend(amount)
+    @spending_money -= amount
+  end
 end
