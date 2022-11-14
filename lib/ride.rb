6df78751299ae19344040501504909cae84cc4f9
Lacey Weaver <jlweave89@gmail.com>
2022-11-14 09:57:13 -0700

    Add board rider method and test

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,7 +3,9 @@ class Ride
               :min_height,
               :admission_fee,
               :excitement,
-              :total_revenue
+              :total_revenue,
+              :rider_log
+              
 
   def initialize(info)
     @name = info[:name]
@@ -11,5 +13,15 @@ class Ride
     @admission_fee = info[:admission_fee]
     @excitement = info[:excitement]
     @total_revenue = 0
+    @rider_log = Hash.new(0)
+  end
+
+  def board_rider(visitor)
+    if visitor.name == visitor.name
+      @rider_log[visitor] += 1
+    else
+      @rider_log[visitor] = 1
+    end
+    @rider_log
   end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,6 +4,8 @@ require './lib/ride'
 RSpec.describe Ride do
   before (:each) do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
   end
 
   describe "#initialize" do
@@ -14,6 +16,19 @@ RSpec.describe Ride do
       expect(@ride1.admission_fee).to eq(1)
       expect(@ride1.excitement).to eq(:gentle)
       expect(@ride1.total_revenue).to eq(0)
+    end
+  end
+
+  describe "#board_rider" do
+    it "can add riders to the rider_log hash" do
+      @visitor1.add_preference(:gentle)
+      @visitor2.add_preference(:gentle)
+
+      @ride1.board_rider(@visitor1)
+      @ride1.board_rider(@visitor2)
+      @ride1.board_rider(@visitor1)
+
+      expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
     end
   end
 end
