8d2e52c0f8af01b06e393fda778b2615be920093
Conner Van Loan <cdvanloan@gmail.com>
2023-01-12 16:59:47 -0700

    Adds initialize test

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,28 +1,4 @@
 class Ride
-	attr_reader :name,
-							:min_height,
-							:admission_fee,
-							:excitement,
-							:total_revenue,
-							:rider_log
 
-	def initialize(info)
-		@name = info[:name]
-		@min_height = info[:min_height]
-		@admission_fee = info[:admission_fee]
-		@excitement = info[:excitement]
-		@total_revenue = 0
-		@rider_log = Hash.new(0)
-	end
-
-	def board_rider(rider)
-		if rider.tall_enough?(min_height) && rider.preferences.include?(excitement)
-			rider_log[rider] += 1
-			@total_revenue += @admission_fee
-			rider.pay_for_ride(@admission_fee)
-		else
-			'Cannot Board'
-		end
-	end
  
 end
--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,26 +1,4 @@
 class Visitor
-	attr_reader :name,
-							:height,
-							:spending_money,
-							:preferences
 
-	def initialize(name, height, spending_money)
-		@name = name
-		@height = height
-		@spending_money = spending_money.gsub('$', '').to_i
-		@preferences = []
-	end
-
-	def add_preference(preference)
-		preferences << preference
-	end
-
-	def tall_enough?(height_needed)
-		height >= height_needed
-	end
-
-	def pay_for_ride(fee)
-		@spending_money -= fee
-	end
 
 end
