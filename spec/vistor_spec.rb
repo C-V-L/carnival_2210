c27dc6faa3c6a96f523ee4e587ed559b318959ac
Conner Van Loan <cdvanloan@gmail.com>
2023-01-12 17:11:16 -0700

    Adds test and method for tall enough

--- spec/vistor_spec.rb
+++ spec/vistor_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe 'Visitor' do
       expect(visitor1.preferences).to eq([:gentle, :water])
     end
   end
+
+  describe '#tall_enough' do
+    it 'can determine if a visitor is tall enough' do
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      expect(visitor1.tall_enough?(54)).to be true
+      expect(visitor2.tall_enough?(54)).to be false
+      expect(visitor3.tall_enough?(54)).to be true
+      expect(visitor1.tall_enough?(64)).to be false
+    end
+  end
 end
